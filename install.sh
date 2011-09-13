f220cc8bf6bd2d1eb79905f9c2712e6966ff3526
mbektimirov <mbektimirov@gmail.com>
2011-09-13 19:52:26 +0700

    edit script again

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-p='PATH=$PATH:~/bin'
-export $p
-echo $p >> ~/.bashrc
+echo 'PATH=$PATH:~/bin' >> ~/.bashrc
 curl https://raw.github.com/n8han/conscript/master/setup.sh | sh
 ~/bin/cs n8han/giter8
